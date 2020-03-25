d8f0ed9db61b22b956a534c5e9c2e4c407c26cda
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-03-25 08:48:43 +0000

    Added code for exercise

---
+++ src/com/qa/main/NumbersRevisited.java
@@ -0,0 +1,12 @@
+package com.qa.main;
+
+public class NumbersRevisited 
+{
+	public void loopFromOneToOneHundred()
+	{
+		for (int loopCount = 1; loopCount < 101; loopCount++)
+		{
+			System.out.println(loopCount);
+		}
+	}
+}
